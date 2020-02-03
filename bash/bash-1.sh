62214e7e1ce7abd972ad34e8355f626020ced670
dslee1371 <dslee1371@gmail.com>
2020-02-03 17:04:43 +0900

    change bash-1.sh

--- bash/bash-1.sh
+++ bash/bash-1.sh
@@ -87,3 +87,11 @@ export KUBE_EDITOR=/usr/bin/vim
 export HELM_HOME=~/.helm
 source <(helm completion bash)
 
+
+# yellow
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;33m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+
